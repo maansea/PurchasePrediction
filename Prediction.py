2892652a88942e4587538f394646c0793059bcaf
maansea <48329378+maansea@users.noreply.github.com>
2022-04-09 15:45:44 +0530

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -11,10 +11,10 @@ from sklearn.ensemble import RandomForestClassifier
 
 def LabelEncoder(input_value, feats):
     feat_value=list(1+np.arange(len(feats)))
-    print(feat_value)
     feat_key=feats
     feat_dict=dict(zip(feat_key,feat_value))
     print(feat_dict)
+    print(input_value)
     value=feat_dict[input_value]
     return value
 
